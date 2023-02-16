935fd60b46d08e306c728000d1024f09b67d5f4c
regulus.wee <catiga03@gmail.com>
2023-02-16 18:30:55 +0800

    1

--- src/pages/multi/tokens.tsx
+++ src/pages/multi/tokens.tsx
@@ -50,8 +50,8 @@ import {
     const { userMint, currentMaxTerm, globalRank, feeData, multiRounds, mintValue } = useContext(XENContext);
     const numberOfDays = 100;
   
-    const quantityOfMint = 128;
-    const [maxMint, setMaxMint] = useState(128);
+    const quantityOfMint = 100;
+    const [maxMint, setMaxMint] = useState(100);
     const [tokenAllowance, setTokenAllowance] = useState<BigNumber>(BigNumber.from(0));
 
     const [approveProcessing, setApproveProcessing] = useState(false);
