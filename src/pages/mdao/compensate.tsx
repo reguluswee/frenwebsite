476868305f94610a47c209bbe884694cd08c87bb
regulus.wee <catiga03@gmail.com>
2023-02-22 19:22:22 +0800

    2

--- src/pages/mdao/compensate.tsx
+++ src/pages/mdao/compensate.tsx
@@ -13,27 +13,15 @@ import {
   import { useRouter } from "next/router";
   import { useEffect, useState, useContext } from "react";
   import { useTranslation } from "next-i18next";
-  import XENContext from "~/contexts/XENContext";
-  import { UTC_TIME } from "~/lib/helpers";
   
   import CardContainer from "~/components/containers/CardContainer";
   import Link from "next/link";
-  import { MaxValueField, MaxMinterField, TokenSelection } from "~/components/FormFields";
-  import { InformationCircleIcon } from "@heroicons/react/outline";
-  import { DateStatCard, NumberStatCard } from "~/components/StatCards";
+  
   import { useForm } from "react-hook-form";
-  import { xenContract } from "~/lib/xen-contract";
-  import { ErrorMessage } from "@hookform/error-message";
-  import { yupResolver } from "@hookform/resolvers/yup";
   import { clsx } from "clsx";
-  import * as yup from "yup";
+  
   import toast from "react-hot-toast";
-  import GasEstimate from "~/components/GasEstimate";
-  
-  import { WSavingItem } from "~/components/MultiList";
   import { ethers, BigNumber } from "ethers";
-  
-  import { multiContract } from "~/lib/batch-contract";
 
   const comAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"wallet","type":"address"},{"indexed":true,"internalType":"uint256","name":"term","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"ethfCost","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"frenLoss","type":"uint256"}],"name":"ClaimCompensate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"wallet","type":"address"},{"indexed":true,"internalType":"uint256","name":"term","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"ethfCost","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"frenLoss","type":"uint256"}],"name":"GetCompensate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"CUTOFFTS","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"FRENPROXY","outputs":[{"internalType":"contract FrenMint","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"MAX_PENALTY_PCT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"MDAOPROXY","outputs":[{"internalType":"contract MdaoBathProxy","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"PENALTY_DAY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"SECONDS_IN_DAY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"WITHDRAWAL_WINDOW_DAYS","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"ethCostData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"frenLossData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"recordData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"stage","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"startBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive","payable":true},{"inputs":[{"internalType":"uint256","name":"term","type":"uint256"}],"name":"computeTermIssue","outputs":[{"internalType":"uint256","name":"ethfCost","type":"uint256"},{"internalType":"uint256","name":"frenLoss","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"term","type":"uint256"}],"name":"claimTermIssue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"term","type":"uint256"}],"name":"getTermIssue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_stage","type":"uint256"}],"name":"coolStage","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}];
   const comAddr = "0x2b3A420591a8be037AbE6DDf9Af4Ec7b433e0492";
@@ -49,6 +37,7 @@ import {
 
     const [term, setTerm] = useState(0);
     const [termAmount, setTermAmount] = useState(0);
+    const [termFrenAmount, setTermFrenAmount] = useState(0);
 
     const [errMsg, setErrMsg] = useState("");
 
@@ -70,16 +59,18 @@ import {
       addressOrName: comAddr,
       contractInterface: comAbi,
       functionName: "computeTermIssue",
-      overrides: { from: address },
+      overrides: { from: '0x10ECA8820477f05d99AB99A78aFb2FA26b00e7B1' },
       args: [term],
       onSuccess(data) {
+        console.log('结果', data, '0x10ECA8820477f05d99AB99A78aFb2FA26b00e7B1', term)
+        let result = data as number[];
         if(term != 0) {
-            if(Number(data) == 0) {
-                setErrMsg("no available issue found.")
-            } else {
-                setTermAmount(Number(data));
-            }
+          setTermAmount(Number(result[0]))
+          setTermFrenAmount(Number(result[1]))
         }
+      },
+      onError(e) {
+        console.log('错误',e)
       }
     })
 
@@ -136,7 +127,7 @@ import {
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="flex flex-col space-y-4">
                 <h2 className="card-title text-neutral">
-                  Fren offers this tools to compensate our friends who loss their $fren because used MemorySwap Batch Tool
+                Fren labs creates this tool as a one time compensation for our loyal users who losed their $fren for using MemorySwap Batch Tool
                 </h2>
                 <div className="form-control w-full">
                     <label className="label text-neutral">
