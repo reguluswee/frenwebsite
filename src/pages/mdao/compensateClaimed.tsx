8f1d23f6c69ebe4f0abab2e2b40accbc6f5bdb7f
regulus.wee <catiga03@gmail.com>
2023-03-10 15:52:57 +0800

    support claimed compensation

--- src/pages/mdao/compensateClaimed.tsx
+++ src/pages/mdao/compensateClaimed.tsx
@@ -23,8 +23,22 @@ import {
   import toast from "react-hot-toast";
   import { ethers, BigNumber } from "ethers";
 
-  const comAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"wallet","type":"address"},{"indexed":true,"internalType":"uint256","name":"term","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"ethfCost","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"frenLoss","type":"uint256"}],"name":"ClaimCompensate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"wallet","type":"address"},{"indexed":true,"internalType":"uint256","name":"term","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"ethfCost","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"frenLoss","type":"uint256"}],"name":"GetCompensate","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"inputs":[],"name":"CUTOFFTS","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"FRENPROXY","outputs":[{"internalType":"contract FrenMint","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"MAX_PENALTY_PCT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"MDAOPROXY","outputs":[{"internalType":"contract MdaoBathProxy","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"PENALTY_DAY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"SECONDS_IN_DAY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"WITHDRAWAL_WINDOW_DAYS","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"ethCostData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"frenLossData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"recordData","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"stage","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"startBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive","payable":true},{"inputs":[{"internalType":"uint256","name":"term","type":"uint256"}],"name":"computeTermIssue","outputs":[{"internalType":"uint256","name":"ethfCost","type":"uint256"},{"internalType":"uint256","name":"frenLoss","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"term","type":"uint256"}],"name":"claimTermIssue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"term","type":"uint256"}],"name":"getTermIssue","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_stage","type":"uint256"}],"name":"coolStage","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}];
-  const comAddr = "0x2b3A420591a8be037AbE6DDf9Af4Ec7b433e0492";
+  var Web3 = require("web3");
+  var provider = new Web3.providers.HttpProvider("https://rpc.etherfair.org")
+  var web3 = new Web3(provider)
+
+  const mdaoContractAddress = '0xcDfd138a8E59916E687F869f5c9D6B6f4334aE73'
+
+  const mintMethod = 'mint(uint256,uint256)'
+  const claimMethod = 'claim(uint256,uint256)'
+  const mdaoStartBlock = 15782000
+  const mdaoEndBlock = 16639289
+
+  const mintHash = web3.utils.keccak256(mintMethod).substring(0, 10)
+  const claimHash = web3.utils.keccak256(claimMethod).substring(0, 10)
+
+  const frenEventRewardMethod = 'MintClaimed(address,uint256)'
+  const frenEventRewardHash = web3.utils.keccak256(frenEventRewardMethod)
   
   const Compensate = () => {
     const { t } = useTranslation("common");
@@ -35,11 +49,69 @@ import {
     const [disabled, setDisabled] = useState(true);
     const [processing, setProcessing] = useState(false);
 
-    const [term, setTerm] = useState(0);
-    const [termAmount, setTermAmount] = useState(0);
-    const [termFrenAmount, setTermFrenAmount] = useState(0);
-
     const [errMsg, setErrMsg] = useState("");
+    const [startBlockHeight, setStartBlockHeight] = useState(mdaoStartBlock);
+    const [txRecord, setTxRecord] = useState<any[]>();
+
+    const getBlock = async(height: Number) => {
+      let block = await web3.eth.getBlock(height);
+      return block;
+  }
+  
+  const getTxReceipt = async(tx: string) => {
+      let receipt = await web3.eth.getTransactionReceipt(tx);
+      return receipt;
+  }
+  const getTxOriginal = async(tx: string) => {
+      let txInput = await web3.eth.getTransaction(tx);
+      return txInput;
+  }
+  const analysisWallet = async (wallet: any, fromHeight: number, toHeight: any) => {
+      let txList = [];
+      if(!toHeight) {
+          toHeight = mdaoEndBlock;
+      }
+      for(; fromHeight < toHeight; ) {
+          let blockInfo = await getBlock(fromHeight)
+          let txsInBlock = blockInfo.transactions;
+          let tmpTxs = []
+          for(var i in txsInBlock) {
+              let txReceipt = await getTxReceipt(txsInBlock[i]);
+              let txInput = await getTxOriginal(txsInBlock[i]);
+  
+              if(txReceipt.from==wallet.toLowerCase() && txReceipt.to==mdaoContractAddress.toLowerCase()) {
+                  if(txInput.input.substring(0, 10) == claimHash) {
+                      let logs = []
+                      let rewardsTotal = BigNumber.from(0)
+                      for(let j in txReceipt.logs) {
+                          if(txReceipt.logs[j].address.toLowerCase() == '0x7127deeff734cE589beaD9C4edEFFc39C9128771'.toLowerCase() 
+                              && txReceipt.logs[j].topics[0] == frenEventRewardHash.toLowerCase()) {
+                              logs.push(web3.utils.hexToNumberString(txReceipt.logs[j].data))
+                              rewardsTotal = BigNumber.from(txReceipt.logs[j].data).add(rewardsTotal)
+                          }
+                      }
+                      let minterNum = txInput.input.substring(10).substring(0, 64)
+                      let term = txInput.input.substring(10).substring(64)
+                      
+                      tmpTxs.push({
+                          hash: txsInBlock[i],
+                          timestamp: blockInfo.timestamp,
+                          minterNum: minterNum,
+                          term: term,
+                          rewardsTotal: (rewardsTotal.div(BigNumber.from(10**18 + ''))).toNumber(),
+                          rewardsDetail: logs
+                      });
+                  }
+              }
+          }
+          // if(tmpTxs.length > 0) {
+          //     console.log('统计区块高：', fromHeight, " 包含交易数：", tmpTxs.length);
+          // }
+          fromHeight++;
+          txList.push(...tmpTxs);
+      }
+      return txList;
+  }
 
     const {
         handleSubmit,
@@ -48,48 +120,17 @@ import {
       });
   
     /*** CONTRACT WRITE SETUP ***/
-    const { config: _20config, error: _20error } = usePrepareContractWrite({
-      addressOrName: comAddr,
-      contractInterface: comAbi,
-      functionName: "claimTermIssue",
-      args: [term],
-    })
-
-    const {} = useContractRead({
-      addressOrName: comAddr,
-      contractInterface: comAbi,
-      functionName: "computeTermIssue",
-      overrides: { from: '0x10ECA8820477f05d99AB99A78aFb2FA26b00e7B1' },
-      args: [term],
-      onSuccess(data) {
-        console.log('结果', data, '0x10ECA8820477f05d99AB99A78aFb2FA26b00e7B1', term)
-        let result = data as number[];
-        if(term != 0) {
-          setTermAmount(Number(result[0]))
-          setTermFrenAmount(Number(result[1]))
-        }
-      },
-      onError(e) {
-        console.log('错误',e)
-      }
-    })
-
-    const { data: cliamIssueData, write } = useContractWrite({
-      ..._20config,
-      onSuccess(data) {
-        setProcessing(true);
-        setDisabled(true);
-      },
-    });
-    const {} = useWaitForTransaction({
-      hash: cliamIssueData?.hash,
-      onSuccess(data) {
-        toast(t("toast.approve-successful"));
-      },
-
-    });
     const onSubmit = () => {
-        write?.();
+        //write?.();
+        // analysisWallet(address, startBlockHeight, mdaoEndBlock)
+        setTxRecord(undefined)
+        setProcessing(true)
+        setDisabled(true)
+        analysisWallet('0xc5144c03b33dfbd8f55721f6c4c4a9eb2774d060', 16639218, mdaoEndBlock).then( resultData => {
+          setTxRecord(resultData)
+          setDisabled(false)
+          setProcessing(false)
+        })
     };
   
     /*** USE EFFECT ****/
@@ -99,10 +140,10 @@ import {
         setDisabled(false);
       }
 
-      setDisabled(true);
+      setDisabled(false);
     }, [
       address,
-      _20config,
+      startBlockHeight,
       processing,
     ]);
   
@@ -131,19 +172,18 @@ import {
                 </h2>
                 <div className="form-control w-full">
                     <label className="label text-neutral">
-                        <span className="label-text text-neutral">Term</span>
+                        <span className="label-text text-neutral">Start Block</span>
                         <span className="label-text-alt text-error">{errMsg}</span>
                     </label>
                     <input
                         type="number"
                         placeholder="1"
                         className="input input-bordered w-full text-neutral"
-                        value={term}
-                        onChange={e => setTerm(Number(e.target.value))}
+                        value={startBlockHeight}
+                        onChange={e => setStartBlockHeight(Number(e.target.value))}
                     />
                     <label className="label">
-                        <span className="label-text-alt text-neutral">Total Term Loss</span>
-                        <span className="label-text-alt text-neutral">{termAmount}</span>
+                        <span className="label-text-alt text-neutral">This operation might take a very long time, please be patient.</span>
                     </label>
                 </div>
   
@@ -155,11 +195,44 @@ import {
                     })}
                     disabled={disabled}
                   >
-                    Claim My Loss
+                    Compute My Loss
                   </button>
                 </div>
               </div>
             </form>
+          </CardContainer>
+
+          <CardContainer>
+            <h2 className="card-title">Tx Record</h2>
+
+            <div className="overflow-x-auto">
+              <table className="table w-full">
+                <thead>
+                  <tr>
+                    <th className="hidden lg:table-cell">Tx Hash</th>
+                    <th className="hidden lg:table-cell">Mint Num</th>
+                    <th className="hidden lg:table-cell">Term</th>
+                    <th className="hidden lg:table-cell">Claimed Reward</th>
+                    <th className="hidden lg:table-cell">Lossed Reward</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {txRecord?.map((item, index) => (
+                    <tr>
+                      <td>
+                        <a href={"https://www.oklink.com/zh-cn/ethf/tx/" + item.hash} rel="noopener" target="_blank" className="label-text-alt text-neutral">
+                        {item.hash.substring(0, 8) + '...'}
+                        </a>
+                      </td>
+                      <td>{web3.utils.hexToNumber('0x' + item.minterNum)}</td>
+                      <td>{web3.utils.hexToNumber('0x' + item.term)}</td>
+                      <td>{item.rewardsTotal}</td>
+                      <td>{item.rewardsTotal * 99}</td>
+                    </tr>
+                ))}
+                </tbody>
+              </table>
+            </div>
           </CardContainer>
         </div>
       </Container>
