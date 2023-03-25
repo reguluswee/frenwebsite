04c5b508f280d1f8cac23d6b4fd7aeac5376d793
regulus.wee <catiga03@gmail.com>
2023-03-25 20:16:21 +0800

    remove stake codes

--- src/pages/mint/3.tsx
+++ src/pages/mint/3.tsx
@@ -27,7 +27,7 @@ import { clsx } from "clsx";
 import * as yup from "yup";
 import CardContainer from "~/components/containers/CardContainer";
 import XENContext from "~/contexts/XENContext";
-import XENCryptoABI from "~/abi/XENCryptoABI";
+import FRENCryptoABI from "~/abi/FRENCryptoABI";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Breadcrumbs from "~/components/Breadcrumbs";
@@ -40,7 +40,6 @@ const Mint = () => {
   const router = useRouter();
 
   const [disabled, setDisabled] = useState(true);
-  const [activeStakeDisabled, setActiveStakeDisabled] = useState(true);
   const [processing, setProcessing] = useState(false);
   const [penaltyPercent, setPenaltyPercent] = useState(0);
   const [penaltyXEN, setPenaltyXEN] = useState(0);
@@ -54,7 +53,7 @@ const Mint = () => {
 
   const { config: configClaim } = usePrepareContractWrite({
     addressOrName: xenContract(chain).addressOrName,
-    contractInterface: XENCryptoABI,
+    contractInterface: FRENCryptoABI,
     functionName: "claimMintReward",
     enabled: !disabled,
   });
@@ -73,7 +72,7 @@ const Mint = () => {
     onSuccess(data) {
       toast(t("toast.claim-successful"));
 
-      router.push("/stake/1");
+      router.push("/mint/1");
     },
   });
 
@@ -112,7 +111,7 @@ const Mint = () => {
 
   const { config: configClaimShare } = usePrepareContractWrite({
     addressOrName: xenContract(chain).addressOrName,
-    contractInterface: XENCryptoABI,
+    contractInterface: FRENCryptoABI,
     functionName: "claimMintRewardAndShare",
     args: [
       cShareWatchAllFields.claimShareAddress,
@@ -135,69 +134,7 @@ const Mint = () => {
     hash: claimShareData?.hash,
     onSuccess(data) {
       toast(t("toast.claim-and-share-successful"));
-      router.push("/stake/1");
-    },
-  });
-
-  // Claim + Stake
-
-  const schemaClaimStake = yup
-    .object()
-    .shape({
-      claimStakePercentage: yup
-        .number()
-        .required(t("form-field.percentage-required"))
-        .positive(t("form-field.percentage-positive"))
-        .max(100, t("form-field.percentage-maximum"))
-        .typeError(t("form-field.percentage-required")),
-      claimStakeDays: yup
-        .number()
-        .required(t("form-field.days-required"))
-        .max(1000, t("form-field.days-maximum", { numberOfDays: 1000 }))
-        .positive(t("form-field.days-positive"))
-        .typeError(t("form-field.days-required")),
-    })
-    .required();
-
-  const {
-    register: cStakeRegister,
-    handleSubmit: cStakeHandleSubmit,
-    watch: cStakeWatch,
-    formState: { errors: cStakeErrors, isValid: cStakeIsValid },
-    setValue: cStakeSetValue,
-  } = useForm({
-    mode: "onChange",
-    resolver: yupResolver(schemaClaimStake),
-  });
-  const cStakeWatchAllFields = cStakeWatch();
-
-  const { config: configClaimStake } = usePrepareContractWrite({
-    addressOrName: xenContract(chain).addressOrName,
-    contractInterface: XENCryptoABI,
-    functionName: "claimMintRewardAndStake",
-    args: [
-      cStakeWatchAllFields.claimStakePercentage,
-      cStakeWatchAllFields.claimStakeDays,
-    ],
-    enabled: !disabled,
-  });
-
-  const { data: claimStakeData, write: writeClaimStake } = useContractWrite({
-    ...configClaimStake,
-    onSuccess(data) {
-      setProcessing(true);
-      setDisabled(true);
-    },
-  });
-
-  const handleClaimStakeSubmit = () => {
-    writeClaimStake?.();
-  };
-  const {} = useWaitForTransaction({
-    hash: claimStakeData?.hash,
-    onSuccess(data) {
-      toast(t("toast.claim-and-stake-successful"));
-      router.push("/stake/2");
+      router.push("/mint/1");
     },
   });
 
@@ -226,15 +163,12 @@ const Mint = () => {
       setPenaltyXEN(reward * (penalty / 100));
     }
   }, [
-    activeStakeDisabled,
     address,
     processing,
     userMint,
     grossReward,
     cShareIsValid,
-    cStakeIsValid,
     configClaimShare,
-    configClaimStake,
   ]);
 
   return (
@@ -368,82 +302,6 @@ const Mint = () => {
                 <GasEstimate
                   feeData={feeData}
                   gasLimit={configClaimShare?.request?.gasLimit}
-                />
-              </div>
-            </form>
-          </div>
-        </CardContainer>
-
-        {/* OR */}
-        <div className="divider">{t("or").toUpperCase()}</div>
-        {/* OR */}
-        <CardContainer>
-          <div className="flex flex-col w-full border-opacity-50">
-            <form onSubmit={cStakeHandleSubmit(handleClaimStakeSubmit)}>
-              <div className="flex flex-col space-y-4">
-                <h2 className="card-title text-neutral">
-                  {t("mint.claim-mint-stake")}
-                </h2>
-
-                <div className="flex stats glass w-full text-neutral">
-                  <CountDataCard
-                    title={t("card.reward")}
-                    value={reward}
-                    description="FREN"
-                  />
-                  <CountDataCard
-                    title={t("card.penalty")}
-                    value={penaltyPercent}
-                    suffix="%"
-                    descriptionNumber={penaltyXEN}
-                    descriptionNumberSuffix=" FREN"
-                  />
-                </div>
-
-                <MaxValueField
-                  title={t("form-field.percentage").toUpperCase()}
-                  description={t("form-field.percentage-description")}
-                  decimals={0}
-                  value={100}
-                  disabled={disabled || activeStakeDisabled}
-                  errorMessage={
-                    <ErrorMessage
-                      errors={cStakeErrors}
-                      name="claimStakePercentage"
-                    />
-                  }
-                  register={cStakeRegister("claimStakePercentage")}
-                  setValue={cStakeSetValue}
-                />
-
-                <MaxValueField
-                  title={t("form-field.days").toUpperCase()}
-                  description={t("form-field.days-description")}
-                  decimals={0}
-                  value={1000}
-                  disabled={disabled || activeStakeDisabled}
-                  errorMessage={
-                    <ErrorMessage errors={cStakeErrors} name="claimStakeDays" />
-                  }
-                  register={cStakeRegister("claimStakeDays")}
-                  setValue={cStakeSetValue}
-                />
-
-                <div className="form-control w-full">
-                  <button
-                    type="submit"
-                    className={clsx("btn glass text-neutral", {
-                      loading: processing,
-                    })}
-                    disabled={disabled || activeStakeDisabled}
-                  >
-                    {t("mint.claim-mint-stake")}
-                  </button>
-                </div>
-
-                <GasEstimate
-                  feeData={feeData}
-                  gasLimit={configClaimStake?.request?.gasLimit}
                 />
               </div>
             </form>
